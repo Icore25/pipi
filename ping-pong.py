84f74dae81e7ea3bec4a46f581015ee6d4faad59
Icore25 <markkovalev564@gmail.com>
2026-05-25 18:49:37 +0300

    Add files via upload

--- ping-pong.py
+++ ping-pong.py
@@ -5,31 +5,41 @@ sos = display.set_mode((677,677))
 
 
 class Player():
-   def __init__(self, x,y,w,h,speed):
+    def __init__(self, x,y,w,h,speed):
        self.hitbox = Rect(x,y,w,h)
-       self.speed = speed
-   def move(self):
-        
+       self.speed = 2
+    def move(self):
         key_list = key.get_pressed()
         if key_list[K_DOWN] == True:
             self.hitbox.y += self.speed
         elif key_list[K_UP] == True:
             self.hitbox.y -= self.speed
 
-   def hit(self):
+    def hit(self):
+       if self.hitbox.colliderect(ball.hitbox): 
+            ball.x_speed = -ball.x_speed
+    def pilot(self):
+        if ball.hitbox.y > self.hitbox.y:
+            self.speed = 2
+        else:
+            self.speed = -2
+        self.hitbox.y += self.speed
+        
+    
 
 
 
-
-player1 = Player(250,250,20,100,5)
+player2 = Player(660,350,20,100,5)
+player1 = Player(0,350,20,100,5)
 red = (150, 10, 10)
-ball = Ball(744,320,4,5,2,3)
+white = (255,255,255)
+
 
 
 
 
 class Ball():
-    def _init_(self, x,y,w,h, x_speed, y_speed):
+    def __init__(self, x,y,w,h, x_speed, y_speed):
         self.hitbox = Rect(x,y,w,h)
         self.x_speed = x_speed
         self. y_speed = y_speed
@@ -37,8 +47,24 @@ class Ball():
         self.hitbox.x += self.x_speed
         self.hitbox. y += self.y_speed
         if self.hitbox.x > 677- self.hitbox.width:
-            self.y_speed = -self.y_speed
             self.x_speed = -self.x_speed
+            self.hitbox.x = 300
+            self.hitbox.y = 300
+
+        if self.hitbox.y > 677- self.hitbox.height:
+            self.y_speed = -self.y_speed
+            
+            
+        if self.hitbox.x < 0 :
+            self.x_speed = -self.x_speed
+            self.hitbox.x = 300
+            self.hitbox.y = 300
+            
+        if self.hitbox.y < 0 :
+            self.y_speed = -self.y_speed
+            
+ball = Ball(300,300,15,15,3,3)
+
 
 
 
@@ -71,15 +97,23 @@ class Ball():
 
 
 while True:
-    
+
+    sos.fill((10,10,100))
     eventlist = event.get()
     draw.rect(sos,red,player1.hitbox)
+    draw.rect(sos,red,player2.hitbox)
     player1.move()
-    sos.fill((10,10,100))
+    player1.hit()
+    player2.pilot()
+    player2.hit()
+    
+    
     for e in eventlist:
         if e.type == QUIT:
             exit()
     draw.rect(sos,white,ball.hitbox)
-    Ball.move()
+    ball.move()
+    
     display.update()
     time.Clock().tick(180)
+    
